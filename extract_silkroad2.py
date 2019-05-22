e0c0967ba8d40b8f00233730d1829fb9a7f6e76f
jeroenatijenv <46672048+jeroenatijenv@users.noreply.github.com>
2019-05-22 17:45:17 +0200

    Update extract_silkroad2.py
    to do: replace vendor where vendor is empty

--- extract_silkroad2.py
+++ extract_silkroad2.py
@@ -26,35 +26,53 @@ def product_to_soup(product):
     return s
     
 
-dir_name = os.listdir()[0]
-products = get_products(dir_name)
-
-# Create empty dataframe
-df = pd.DataFrame()
-
-# Loop through products in directory
-for product in products:
-    soup = product_to_soup(product)
+for dir_name in [i for i in os.listdir() if i.startswith('201')]:
+    products = get_products(dir_name)
     
-    titles = [i.text for i in soup.select('div.item_title')]
-    prices = [i.text for i in soup.select('div.price')]
-    shipping = [i.text.strip().split('\n') for i in soup.select('div.shipping')]
-    ships_from = [i[0].strip().split()[2] for i in shipping]
-    ships_to = [i[1].strip().split()[2] for i in shipping]
-    rating_count = [i.text[1:-1] for i in soup.select('div.rating_count')]
-    member_name = [i.text for i in soup.select('div.vendor a')]
-    member_rating = [i.text for i in soup.select('span.vendor_rating')]
+    # Create empty dataframe
+    df = pd.DataFrame()
     
-    _df = pd.DataFrame({
-        'product':titles,
-        'prices':prices,
-        'ships_from':ships_from,
-        'ships_to':ships_to,
-        'rating_count':rating_count,
-        'member_name':member_name,
-        'member_rating':member_rating
-        })
+    # Loop through products in directory
+    for product in products:
+        soup = product_to_soup(product)
 
-    df = df.append(_df)
-
-df.reset_index(inplace=True, drop=True)
+        for i in [vendor.text for vendor in soup.select('div.vendor')]:
+            if i.find('sold by \n') != 1:
+                #DO SOMETHING TO REPLACE I WITH -
+        
+        try:
+            titles = [i.text for i in soup.select('div.item_title')]
+            prices = [i.text for i in soup.select('div.price')]
+            shipping = [i.text.strip().split('\n') for i in soup.select('div.shipping')]
+            ships_from = [i[0].strip().split()[2] for i in shipping]
+            ships_to = [i[1].strip().split()[2] for i in shipping]
+            rating_count = [i.text[1:-1] for i in soup.select('div.rating_count')]
+            member_name = [i.text for i in soup.select('div.vendor a')]
+            member_rating = [i.text for i in soup.select('span.vendor_rating')]
+            
+            # not all html follow same structure
+            if not member_rating:
+                titles = [i.text for i in soup.select('div.item_title')]
+                prices = [i.text for i in soup.select('div.price_big')]
+                item_details = [i.text.strip().split('\n') for i in soup.select('div.item_details')]
+                member_name = [i[0][8:] for i in item_details]
+                ships_from = [i[1].strip()[12:] for i in item_details]
+                ships_to = [i[2].strip()[10:] for i in item_details]
+                rating_count = ['nan' for i in titles] #not existing in this data
+                member_rating = ['nan' for i in titles] #not existing in this data
+        except IndexError:
+            pass
+        
+        _df = pd.DataFrame({
+            'product':titles,
+            'prices':prices,
+            'ships_from':ships_from,
+            'ships_to':ships_to,
+            'rating_count':rating_count,
+            'member_name':member_name,
+            'member_rating':member_rating
+            })
+    
+        df = df.append(_df)
+    
+    df.reset_index(inplace=True, drop=True)

fb655add20c6ed754421c9effdefb2fc70413a56
jeroenatijenv <46672048+jeroenatijenv@users.noreply.github.com>
2019-05-28 16:24:31 +0200

    Handle missing data, include try/excepts

--- extract_silkroad2.py
+++ extract_silkroad2.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 import pandas as pd
 from bs4 import BeautifulSoup as bs
+import traceback
 
 
 def get_products(dir_name, amount=np.inf):
@@ -19,33 +20,39 @@ def get_products(dir_name, amount=np.inf):
         return products[:amount]
 
 
-def product_to_soup(product):
-    '''open product file, create beautifulsoup'''
-    p = open(product, 'rb').read()
-    s = bs(p, 'lxml')
-    return s
-    
+# Create iterator (i) to check the amount of iterations completed
+i = 0
 
+# Create empty dataframe for the data in all directories
+df_full = pd.DataFrame()
+
+# Loop over all subdirectories in directory
 for dir_name in [i for i in os.listdir() if i.startswith('201')]:
+    # Get list of filenames of products in subdirectory
     products = get_products(dir_name)
     
-    # Create empty dataframe
-    df = pd.DataFrame()
+    # Create empty dataframe for the data in this directory
+    df_dir = pd.DataFrame()
     
     # Loop through products in directory
     for product in products:
-        soup = product_to_soup(product)
+        p = open(product, 'r').read()
 
-        for i in [vendor.text for vendor in soup.select('div.vendor')]:
-            if i.find('sold by \n') != 1:
-                #DO SOMETHING TO REPLACE I WITH -
+        # If there are any unknown seller names, replace empty value with '-'
+        if p.find('sold by \n') != -1:
+            p = p.replace('sold by \n', 'sold by <a href="unknown">-</a> \n')
+
+        # Make soup with BeautifulSoup
+        soup = bs(p, 'lxml')
+
         
+        # find relevant information in html text
         try:
             titles = [i.text for i in soup.select('div.item_title')]
             prices = [i.text for i in soup.select('div.price')]
             shipping = [i.text.strip().split('\n') for i in soup.select('div.shipping')]
-            ships_from = [i[0].strip().split()[2] for i in shipping]
-            ships_to = [i[1].strip().split()[2] for i in shipping]
+            ships_from = [i[0].split(':')[1] for i in shipping]
+            ships_to = [i[1].split(':')[1].strip() for i in shipping]
             rating_count = [i.text[1:-1] for i in soup.select('div.rating_count')]
             member_name = [i.text for i in soup.select('div.vendor a')]
             member_rating = [i.text for i in soup.select('span.vendor_rating')]
@@ -63,16 +70,36 @@ for dir_name in [i for i in os.listdir() if i.startswith('201')]:
         except IndexError:
             pass
         
-        _df = pd.DataFrame({
-            'product':titles,
-            'prices':prices,
-            'ships_from':ships_from,
-            'ships_to':ships_to,
-            'rating_count':rating_count,
-            'member_name':member_name,
-            'member_rating':member_rating
-            })
+        try:
+            # Put data in dataframe
+            _df = pd.DataFrame({
+                'product':titles,
+                'prices':prices,
+                'ships_from':ships_from,
+                'ships_to':ships_to,
+                'rating_count':rating_count,
+                'member_name':member_name,
+                'member_rating':member_rating
+                })
+        except ValueError:
+            # Print error when missing data results in size differences of arrays
+            print('ValueError in: ' + product + '\n' + traceback.format_exc())
+            continue
     
-        df = df.append(_df)
+        # Append dataframe to dataframe containing all data of the subdirectory
+        df_dir = df_dir.append(_df)
     
-    df.reset_index(inplace=True, drop=True)
+    # Append dataframe with subdirectory data to dataframe with all data in directory
+    df_full = df_full.append(df_dir)
+    df_full.reset_index(inplace=True, drop=True)
+
+    i = i+1
+    print('directory processed: ' + str(i) + '/' + str(len(os.listdir())) )
+
+
+# Optionally save to csv file
+save_to_csv = input('save output to csv? [y/n]: ')
+if save_to_csv == 'y':
+    df_full.to_csv('silkroad2_allproducts.csv')
+else:
+    print('Script completed, output not saved')
